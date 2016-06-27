e87befa1bf7d6827cccdce2246c01d41e0362794
hpaasch <hepaasch@gmail.com>
2016-06-27 14:37:09 -0400

    first call of listing people works.

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import requests
 
-url = "http://swapi.co/api/people"
 swapi_dict = {
   1: "http://swapi.co/api/people/",
-  2: "http://swapi.co/api/planets/",
-  3: "http://swapi.co/api/films/",
-  4: "http://swapi.co/api/species/",
-  5: "http://swapi.co/api/vehicles/",
-  6: "http://swapi.co/api/starships/",
+  # 2: "http://swapi.co/api/planets/",
+  2: "http://swapi.co/api/films/",
+  # 4: "http://swapi.co/api/species/",
+  3: "http://swapi.co/api/vehicles/",
+  # 6: "http://swapi.co/api/starships/",
 }
 
-response = requests.get(url).json()
+choice = int(input("""
+1 Characters
+2 Films
+3 Vehicles
+"""))
 
-choice = int(input("1 for People, 2 for Planets "))
+url = "http://swapi.co/api/people"
 
 if choice == 1:
-    for people in response['results']:
-        print(people['name'])
-elif choice == 2:
-    for planets in response['results']:
-        print(planets['name'])
+    response = requests.get(url).json()
+    if response['next']:
+        while response['next']:
+            for item in response['results']:
+                print(item['name'])
+            url = response['next']
+            response = requests.get(url).json()
+    else:
+        for item in response['results']:
+            print(item['name'])
+
+
+
+# would you like to get more results? like to see some detail? type 1
