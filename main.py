fd6e45a1f9c5c95c7f39eaf0f80531cae8d95197
hpaasch <hepaasch@gmail.com>
2016-06-27 16:34:26 -0400

    first refactor done. starting detail.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import requests
+import sys
+
+def results(response, look_up):
+    if response['next']:
+        while response['next']:
+            for item in response['results']:
+                print(item[look_up])
+            url = response['next']
+            response = requests.get(url).json()
+    else:
+        for item in response['results']:
+            print(item[look_up])
+
+def get_swapi_data(choice):
+    url, look_up = swapi_dict[choice]
+    response = requests.get(url).json()
+    results(response, look_up)
 
 swapi_dict = {
   1: ["http://swapi.co/api/people/", "name"],
@@ -9,60 +26,23 @@ swapi_dict = {
   # 6: "http://swapi.co/api/starships/",
 }
 while True:
-    overall_choice = int(input("""
+    choice = int(input("""
     1 Characters
     2 Films
     3 Vehicles
     0 Exit
     """))
 
-    if overall_choice == 1:
-        url, look_up = swapi_dict[overall_choice]
-        response = requests.get(url).json()
-        if response['next']:
-            while response['next']:
-                for item in response['results']:
-                    print(item[look_up])
-                url = response['next']
-                response = requests.get(url).json()
-        else:
-            for item in response['results']:
-                print(item[look_up])
-        more_info = input("Would you like details on a character? Y/n ").lower()
-        if more_info == 'y':
-            person = input("Type in the character's name: ").lower()
-            print(response['name'])
-
-    if overall_choice == 2:
-        url, look_up = swapi_dict[overall_choice]
-        response = requests.get(url).json()
-        if response['next']:
-            while response['next']:
-                for item in response['results']:
-                    print(item[look_up])
-                url = response['next']
-                response = requests.get(url).json()
-        else:
-            for item in response['results']:
-                print(item[look_up])
-
-    if overall_choice == 3:
-        url, look_up = swapi_dict[overall_choice]
-        response = requests.get(url).json()
-        if response['next']:
-            while response['next']:
-                for item in response['results']:
-                    print(item[look_up])
-                url = response['next']
-                response = requests.get(url).json()
-        else:
-            for item in response['results']:
-                print(item[look_up])
-
-    if overall_choice == 0:
-        break
+    if choice == 0:
+        exit()
+    get_swapi_data(choice)
+    
 
 
+        # more_info = input("Would you like details on a character? Y/n ").lower()
+        # if more_info == 'y':
+        #     person = input("Type in the character's name: ").lower()
+        #     print(response['name'])
 
 
 
